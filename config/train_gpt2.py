ed9a83cd0d572ccf31154c225d5da5a0295daf50
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2025-12-07 20:51:17 +0000

    gpt-2 config updated

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -7,19 +7,20 @@ wandb_project = 'owt'
 wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
-# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
+# 12 batch size * 1024 block size * 20 gradaccum * 2 GPUs = 491,520
 batch_size = 12
 block_size = 1024
-gradient_accumulation_steps = 5 * 8
+gradient_accumulation_steps = 20 * 2
 
-# this makes total number of tokens be 300B
-max_iters = 600000
-lr_decay_iters = 600000
+# this makes total number of tokens be 30B
+max_iters = 60000
+lr_decay_iters = 60000
 
 # eval stuff
 eval_interval = 1000
 eval_iters = 200
 log_interval = 10
+gated_attn = False
 
 # weight decay
 weight_decay = 1e-1
